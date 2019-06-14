5f67972b679ba30a08c5035c3318d9aa97c34d5f
Alyssa Farina <alyssafarina695@gmail.com>
2019-06-14 04:34:41 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
+it('defines greeting', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
+it('concatenates strings to greet a special guest in greetSpecialGuest', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
-it('interpolates a string in `conversation`', function() {
+it('interpolates a string in conversation', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
